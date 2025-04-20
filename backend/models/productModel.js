7c4bd259da704460884213cdca2e5d383646586a
sidhantrajput1 <sidhant808310@gmail.com>
2025-04-20 08:00:36 +0530

    Product added functionality added

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema({
   image: { type: Array, required: true },
   category : { type : String, required : true },
   subCategory : {type : String, required : true },
-  sizes : {type : Number, required : true },
+  sizes : {type : Array, required : true },
   bestseller : { type : Boolean },
   date : { type : Number, required : true }
 }, {timestamps : true });
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import  'dotenv/config'
 import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import userRouter from './routes/userRoutes.js';
+import productRouter from './routes/productRoutes.js';
 
 // App config
 const app = express();
@@ -20,6 +21,7 @@ connectCloudinary()
 
 // api end point
 app.use('/api/v1/user', userRouter)
+app.use('/api/v1/product', productRouter)
 
 app.get('/', (req, res) => {
     res.send("Hello from backend")
